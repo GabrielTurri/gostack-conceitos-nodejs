3d49054caf1fa51ebb1c0fdbc591f7cbc8101e70
Gabriel Turri <ga-briel.t.a@hotmail.com>
2020-08-29 14:26:49 -0300

    Update2908

--- src/app.js
+++ src/app.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const e = require("express");
-
 const { uuid } = require("uuidv4");
 
 const app = express();
@@ -19,7 +17,6 @@ app.get("/repositories", (request, response) => {
 //CREATE
 app.post("/repositories", (request, response) => {
   const { title, url, techs } = request.body;
-  console.log(title);
   
   const repository = { id: uuid(), title, url, techs, likes: 0 };
 
@@ -33,25 +30,23 @@ app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
   const { title, url, techs } = request.body;
 
-  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
-
-  if (repositoryIndex < 0){
-    return response.status(400).json({ error: 'Repository not found'});
-  }
+  const repositoryIndex = repositories.findIndex(repo => repo.id === id);
   
+  if (repositoryIndex < 0)
+    return response.status(400).json({ error: 'Repository not found'});
 
+  const like = repositories[repositoryIndex].likes;
   const repository = {
     id,
     title,
     url,
     techs,
+    like
   };
-  
 
   repositories[repositoryIndex] = repository;
 
   return response.json(repository);
-
 });
 
 app.delete("/repositories/:id", (request, response) => {
@@ -72,10 +67,9 @@ app.post("/repositories/:id/like", (request, response) => {
 
   const repository = repositories.find(repository => repository.id === id);
 
-  if (!repository){
+  if (!repository)
     return response.status(400).json({ error: 'Repository not found'});
-  }
-
+  
   repository.likes++;
 
   repositories[repository] = repository;
